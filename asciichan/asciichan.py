93a55d9ac96b21338ad5ecdab8801f07671aec38
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2017-06-01 10:42:19 -0700

    Add database functionality, styling still does not work

--- asciichan/asciichan.py
+++ asciichan/asciichan.py
@@ -18,10 +18,15 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-        
+class Art(db.Model):
+       title = db.StringProperty(required = True)
+       art = db.TextProperty(required = True)
+       created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
-        self.render("front.html", title=title, art=art, error=error)
+        arts = db.GqlQuery("SELECT * from Art ORDER BY created DESC")
+        self.render("front.html", title=title, art=art, error=error, arts=arts)
             
     def get(self):
         self.render_front()
@@ -31,7 +36,9 @@ class MainPage(Handler):
         art = self.request.get("art")
 
         if title and art:
-            self.write("Thanks!")
+            a = Art(title = title, art = art)
+            a.put()
+            self.redirect('/')
         else:
             error = "we need a title and art!"
             self.render_front(title, art, error)
