5d1beb83c6293a79c8ebb59d10006b2af0762b93
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2017-06-06 18:50:51 -0700

    Add registration capability

--- blog.py
+++ blog.py
@@ -1,5 +1,8 @@
 import os
 import re
+import hashlib
+import hmac
+import random
 import webapp2
 import jinja2
 from string import letters
@@ -9,10 +12,20 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
+secret = 'dakfjnasjkfnjkasnfjkaoqionqkqnfk'
+
+
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+def make_secure_val(val):
+    return val + "|" + hmac.new(secret, val).hexdigest()
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -22,6 +35,67 @@ class BlogHandler(webapp2.RequestHandler):
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return salt + "," + h
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+def users_key(group = 'default'):
+    return db.Key.from_path('users', group)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent = users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name=', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent = users_key(), name = name, pw_hash = pw_hash, email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
@@ -67,7 +141,34 @@ class Signup(BlogHandler):
         if have_error:
             self.render('signup-form.html', **params)
         else:
-            self.redirect('/welcome?username=' + username)
+            self.done()
+
+    def done(selfi, *a, **kw):
+        raise NotImplementedError
+
+class Unit2Signup(Signup):
+    def done(self):
+        self.redirect('/welcome?username=' + self.username)
+
+class Register(Signup):
+    def done(self):
+        #make sure user doesn't already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists'
+            self.render('signupform.html', error_usernmae = msg)
+        else:
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+            self.login(u)
+            self.redirect('/welcome')
+
+class Unit3Welcome(BlogHandler):
+    def get(self):
+        if self.user:
+            self.render('welcome.html', username = self.user.name)
+        else:
+            self.redirect('/signup')
 
 class Welcome(BlogHandler):
     def get(self):
